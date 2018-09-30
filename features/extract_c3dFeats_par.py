71c68f66c3a33a2a0fdd6619bca7738982eb82a5
arpane4c5 <arpan.e4c5@gmail.com>
2018-09-30 23:30:20 +0530

    extract_c3dFeats_par.py and model_c3d.py for extracting fc7 features from dataset videos. Pretrained wts saved externally.

--- features/extract_c3dFeats_par.py
+++ features/extract_c3dFeats_par.py
@@ -3,9 +3,10 @@
 """
 Created on Sat Sept 8 01:34:25 2018
 @author: Arpan
-@Description: Utils file to extract frame from folder videos and dump to disk.
-Feature : Frames converted to numpy files, easy to subset consecutive frames 
-and feed them to the deep network.
+@Description: Utils file to extract c3d features from dataset videos and dump 
+to disk.
+Feature : C3D features extracted from the videos, taken pretrained model (on Sports-1M)
+and extracting fc7 layer feature vectors. (4096 size)
 """
 
 import torch
@@ -17,9 +18,9 @@ import pandas as pd
 import torch.nn as nn
 from torch.autograd import Variable
 from joblib import Parallel, delayed
-    
 
-def extract_c3d_all(model, srcFolderPath, destFolderPath, onGPU=True, depth=16, stop='all'):
+def extract_c3d_all(model, srcFolderPath, destFolderPath, njobs=1, batch=10, \
+                    onGPU=True, depth=16, stop='all'):
     """
     Function to extract the features from a list of videos
     
@@ -43,43 +44,45 @@ def extract_c3d_all(model, srcFolderPath, destFolderPath, onGPU=True, depth=16, 
     ------
     traversed: int
         no of videos traversed successfully
-    """
+    """    
     # iterate over the subfolders in srcFolderPath and extract for each video 
-    vfiles = os.listdir(srcFolderPath)
-    
+    sfp_lst = os.listdir(srcFolderPath)
     infiles, outfiles, nFrames = [], [], []
-    
-    traversed = 0
-    # create destination path to store the files
-    if not os.path.exists(destFolderPath):
-        os.makedirs(destFolderPath)
+    traversed_tot = 0
+    for sf in sfp_lst:
+        traversed = 0
+        sub_src_path = os.path.join(srcFolderPath, sf)
+        sub_dest_path = os.path.join(destFolderPath, sf)
+        if os.path.isdir(sub_src_path):
+            # create destination path to store the files
+            if not os.path.exists(sub_dest_path):
+                os.makedirs(sub_dest_path)
             
-    # iterate over the video files inside the directory sf
-    for vid in vfiles:
-        if os.path.isfile(os.path.join(srcFolderPath, vid)) and vid.rsplit('.', 1)[1] in {'avi', 'mp4'}:
-            infiles.append(os.path.join(srcFolderPath, vid))
-            outfiles.append(os.path.join(destFolderPath, vid.rsplit('.', 1)[0]+".npy"))
-            nFrames.append(getTotalFramesVid(os.path.join(srcFolderPath, vid)))
-            # save at the destination, if extracted successfully
-            traversed += 1
-#            print "Done "+str(traversed_tot+traversed)+" : "+sf+"/"+vid
-                    
-                # to stop after successful traversal of 2 videos, if stop != 'all'
-            if stop != 'all' and traversed == stop:
-                break
-                    
-    print "No. of files to be written to destination : "+str(traversed)
-    if traversed == 0:
+            # iterate over the video files inside the directory sf
+            vfiles = os.listdir(sub_src_path)
+            for vid in vfiles:
+                if os.path.isfile(os.path.join(sub_src_path, vid)) and vid.rsplit('.', 1)[1] in {'avi', 'mp4'}:
+                    infiles.append(os.path.join(sub_src_path, vid))
+                    outfiles.append(os.path.join(sub_dest_path, vid.rsplit('.',1)[0]+".npy"))
+                    nFrames.append(getTotalFramesVid(os.path.join(sub_src_path, vid)))
+                    traversed += 1
+#                   print "Done "+str(traversed_tot+traversed)+" : "+sf+"/"+vid                    
+                    # to stop after successful traversal of 2 videos, if stop != 'all'
+                    if stop != 'all' and traversed == stop:
+                        break
+            traversed_tot += traversed
+
+    print "No. of files to be written to destination : "+str(traversed_tot)
+    if traversed_tot == 0:
         print "Check the structure of the dataset folders !!"
-        return traversed
+        return traversed_tot
+
     ###########################################################################
     #### Form the pandas Dataframe and parallelize over the files.
     filenames_df = pd.DataFrame({"infiles":infiles, "outfiles": outfiles, "nframes": nFrames})
     filenames_df = filenames_df.sort_values(["nframes"], ascending=[True])
     filenames_df = filenames_df.reset_index(drop=True)
     nrows = filenames_df.shape[0]
-    batch = 5  # No. of videos in a single batch
-    njobs = 5   # No. of threads
     
     ###########################################################################
     if onGPU:
@@ -151,51 +154,6 @@ def getTotalFramesVid(srcVideoPath):
     tot_frames = cap.get(cv2.CAP_PROP_FRAME_COUNT)
     cap.release()
     return tot_frames    
-
-
-def getFrames(srcVideoPath):
-    """
-    Function to read all the frames of the video file into a single numpy matrix
-    and return that matrix to the caller. This function can be called parallely 
-    based on the amount of memory available.
-    """
-    # get the VideoCapture object
-    cap = cv2.VideoCapture(srcVideoPath)
-    
-    # if the videoCapture object is not opened then exit without traceback
-    if not cap.isOpened():
-        print("Error reading the video file !!")
-        return None
-    
-    w, h = (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)),int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
-    #totalFrames = cap.get(cv2.CAP_PROP_FRAME_COUNT)
-    frameCount = 0
-    features_current_file = []
-    
-    #ret, prev_frame = cap.read()
-    assert cap.isOpened(), "Capture object does not return a frame!"
-    #prev_frame = cv2.cvtColor(prev_frame, cv2.COLOR_BGR2GRAY)
-    
-    # Iterate over the entire video to get the optical flow features.
-    while(cap.isOpened()):
-        frameCount +=1
-        ret, curr_frame = cap.read()
-        if not ret:
-            break
-        curr_frame = cv2.cvtColor(curr_frame, cv2.COLOR_BGR2GRAY)
-        # taking centre crop of 112 x 112
-        curr_frame = curr_frame[(w/2-56):(w/2+56), (h/2-56):(h/2+56)]
-        #flow = cv2.calcOpticalFlowFarneback(prev_frame,curr_frame, None, 0.5, 3, 15, 3, 5, 1.2, 0)
-        #hog_feature = hog.compute(curr_frame)   # vector
-        # saving as a list of float values (after converting into 1D array)
-        #features_current_file.append(hog_feature.ravel().tolist())
-        features_current_file.append(curr_frame)
-
-    # When everything done, release the capture
-    cap.release()
-    #print "{}/{} frames in {}".format(frameCount, totalFrames, srcVideoPath)
-    #return features_current_file
-    return np.array(features_current_file)      # convert to N x w x h
 
 
 def getC3DFrameFeats(model, srcVideoPath, onGPU, depth):
@@ -297,8 +255,21 @@ def getC3DFrameFeats(model, srcVideoPath, onGPU, depth):
 
 
 if __name__=='__main__':
-    onGPU = False    # Flag True if we want a GPU extract (Serial),
+
+    # The srcPath should have subfolders that contain the videos.
+    weights_path = "/home/hadoop/VisionWorkspace/Cricket/localization_rnn/c3d.pickle"
+    srcPath = "/home/hadoop/VisionWorkspace/Cricket/dataset_25_fps"
+    destPath = "/home/hadoop/VisionWorkspace/Cricket/scripts/extracted_features/c3d_feats_16"
+    if not os.path.exists(srcPath):
+        srcPath = "/home/arpan/DATA_Drive/Cricket/dataset_25_fps"
+        destPath = "/home/arpan/VisionWorkspace/shot_detection/extracted_features/c3d_feats_16"
+        weights_path = "/home/arpan/VisionWorkspace/localization_rnn/c3d.pickle"
+    
+    onGPU = True    # Flag True if we want a GPU extract (Serial),
     # False if we want a parallel extraction on the CPU cores.
+    
+    batch = 5  # No. of videos in a single batch
+    njobs = 1   # No. of threads    
     
     # create a model 
     import model_c3d as c3d
@@ -308,24 +279,16 @@ if __name__=='__main__':
     ###########################################################################
     
     # get network pretrained model
-    model.load_state_dict(torch.load('c3d.pickle'))
+    model.load_state_dict(torch.load(weights_path))
     if onGPU:
         model.cuda()
         
     model.eval()
-
-    # The srcPath should have subfolders that contain the training, val, test videos.
-    #srcPath = '/home/arpan/DATA_Drive/Cricket/dataset_25_fps'
-    srcPath = "/home/hadoop/VisionWorkspace/VideoData/sample_cricket/ICC WT20"
-    destPath = "/home/hadoop/VisionWorkspace/Cricket/localization_rnn/c3d_feats_16_cpu"
-    if not os.path.exists(srcPath):
-        srcPath = "/opt/datasets/cricket/ICC_WT20"
-        destPath = "/home/arpan/VisionWorkspace/localization_rnn/c3d_feats_16_cpu"
-    
     
     print "Using the GPU : "+str(onGPU)
     start = time.time()
-    nfiles = extract_c3d_all(model, srcPath, destPath, onGPU=onGPU, depth=16, stop='all')
+    nfiles = extract_c3d_all(model, srcPath, destPath, njobs=njobs, \
+                             batch=batch, onGPU=onGPU, depth=16, stop=2)
     end = time.time()
     print "Total no. of files traversed : "+str(nfiles)
     print "Total execution time : "+str(end-start)
@@ -334,7 +297,7 @@ if __name__=='__main__':
     # Results:
     # Data kept on the GPU and the frames are sent to the GPU and appended by 
     # sliding the window of size 16 on the GPU. The model is saved on the GPU
-    # initially and a forward pass through the network gives the FC7 features vector.
+    # initially and a forward pass through the network gives the FC7 feature vectors.
     #
     # On GPU: Serial Execution time(26 vids) : 14497.0 sec
     #
